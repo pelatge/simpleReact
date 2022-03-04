f2ac61c7f004182f3cd39d9d9db0a1cdd2dca80b
pelatge <pelat.g29@gmail.com>
2022-03-04 13:52:49 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Redirect
+  Link,
+  Outlet
 } from "react-router-dom";
 import {Home,Blogs,About,ReduxView,ViewApi} from './Pages';
 
@@ -13,15 +14,69 @@ function App(){
       <>
       <Router>
       <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/blogs" element={<Blogs/>}/>
-      <Route path="/reduxview" element={<ReduxView />}/>
-      <Route path="/viewapi" element={<ViewApi />}/>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>} />
+        <Route path="blogs" element={<Blogs/>}/>
+        <Route path="reduxview" element={<ReduxView />}/>
+        <Route path="viewapi" element={<ViewApi />}/>
+      </Route>
       </Routes>
       </Router>
       </>
     );
   
 }
+
+
+function Layout() {
+  const [collapseShow, setCollapseShow] = React.useState("hidden");
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
+      <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+          {/* Toggler */}
+          <button
+            className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+            type="button"
+            onClick={() => setCollapseShow("bg-white m-2 py-3 px-6")}
+          >
+            <i className="fas fa-bars">==</i>
+          </button>
+          {/* Brand */}
+          <Link
+            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            to="/"
+          >
+            simpleReact
+          </Link>
+        <ul className="md:flex-col md:min-w-full flex flex-col list-none">
+          <li className="items-center">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">About</Link>
+          </li>
+          <li>
+            <Link to="/reduxview">Redux</Link>
+          </li>
+          <li>
+            <Link to="/viewapi">Axios</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
 
 export default hot(App);
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(){
 	return(
 		<div>
-		<h1 className="bg-red-500">Welcome to simpleReact Homepage</h1>
+		<h1 className="bg-red-500 text-white">Welcome to simpleReact Homepage</h1>
 		</div>
 	);
 }
